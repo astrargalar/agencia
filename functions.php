2057aeb37d2f6110ac391f8d593122dcbd0ffdd3
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-07-26 21:07:26 +0200

    Encabezado terminado a falta del @media

--- functions.php
+++ functions.php
@@ -7,3 +7,6 @@ function load_theme_stylesheet()
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'load_theme_stylesheet');
+//Habilitar modo oscuro del editor
+add_theme_support('editor-styles');
+add_theme_support('dark-editor-style');
