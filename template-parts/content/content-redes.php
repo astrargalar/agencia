b40a2e0dad4c7ccb5b5ac146f8f481bba46d76f3
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-08-06 17:22:50 +0200

    Creo template-part con las redes sociales para poder reutilizarlo en más páginas

--- template-parts/content/content-redes.php
+++ template-parts/content/content-redes.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Template part for displaying post archives and search results
+ * Template part for displaying the social network
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -12,7 +12,6 @@
 
 ?>
 <div class="redes">
-
 	<a href="https://www.youtube.com/channel/UChRx8eCuQ_40ixbxjErm1rA" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/youtube.png" alt="sigue a El taller de Astrargalar en Youtube" title="sigue a El taller de Astrargalar en Youtube"></a>
 	<a href="https://www.linkedin.com/in/francisco-silva-richarte-03030219/" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/linkedin.png" alt="sigue a El taller de Astrargalar LinkedIn" title="sigue a El taller de Astrargalar LinkedIn"></a>
 	<a href="https://www.facebook.com/100026348976151" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/facebook.png" alt="sigue a El taller de Astrargalar en Facebook" title="sigue a El taller de Astrargalar en Facebook"></a>
