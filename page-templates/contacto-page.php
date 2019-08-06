36704eb162ebd0ccb8061a82a301f918f703c2d8
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-08-06 17:21:03 +0200

    Creo template-part con las redes sociales para poder reutilizarlo en más páginas

--- page-templates/contacto-page.php
+++ page-templates/contacto-page.php
@@ -23,21 +23,16 @@ get_header();
 	<div class="column active">
 		<h2>Aquí nos puedes encontrar</h2>
 		<div class="content-map">
-
 			<div id="mapid" class="map"></div>
 			<div class="direcciones">
-				<div class="redes"><a href="https://www.facebook.com/100026348976151" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/facebook.png" alt="sigue a El taller de Astrargalar en Facebook" title="sigue a El taller de Astrargalar en Facebook"></a>
-					<a href="https://twitter.com/astrargalar" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/twitter.png" alt="sigue a El taller de Astrargalar Twitter" title="sigue a El taller de Astrargalar Twitter"></a>
-					<a href="https://www.youtube.com/channel/UChRx8eCuQ_40ixbxjErm1rA" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/youtube.png" alt="sigue a El taller de Astrargalar en Youtube" title="sigue a El taller de Astrargalar en Youtube"></a>
-					<a href="https://www.linkedin.com/in/francisco-silva-richarte-03030219/" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/linkedin.png" alt="sigue a El taller de Astrargalar LinkedIn" title="sigue a El taller de Astrargalar LinkedIn"></a>
-				</div>
+				<?php get_template_part('template-parts/content/content-redes'); ?>
+				<!-- Agrego el contenido de las redes sociales en un template part para poder reutilizarlo -->
 				<hr>
 				<p>Cádiz - Avenida de las Cortes de Cádiz - 11005</p>
 				<hr>
 				<p>Chiclana - Avenida Severo Ochoa s/n - 11130</p>
 				<hr>
 			</div>
-
 		</div>
 		<?php get_template_part('template-parts/content/content-loop'); ?>
 	</div>
---
+++ template-parts/content/content-redes.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Template part for displaying post archives and search results
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+<div class="redes">
+
+	<a href="https://www.youtube.com/channel/UChRx8eCuQ_40ixbxjErm1rA" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/youtube.png" alt="sigue a El taller de Astrargalar en Youtube" title="sigue a El taller de Astrargalar en Youtube"></a>
+	<a href="https://www.linkedin.com/in/francisco-silva-richarte-03030219/" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/linkedin.png" alt="sigue a El taller de Astrargalar LinkedIn" title="sigue a El taller de Astrargalar LinkedIn"></a>
+	<a href="https://www.facebook.com/100026348976151" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/facebook.png" alt="sigue a El taller de Astrargalar en Facebook" title="sigue a El taller de Astrargalar en Facebook"></a>
+	<a href="https://twitter.com/astrargalar" rel="nofollow"><img src="http://localhost/tswp/wp-content/uploads/2019/08/twitter.png" alt="sigue a El taller de Astrargalar Twitter" title="sigue a El taller de Astrargalar Twitter"></a>
+</div>
