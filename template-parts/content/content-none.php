260fc9350b4556198cc467416f8fbac76a326005
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-08-30 14:20:32 +0200

    Optimizamos estilos

--- template-parts/content/content-none.php
+++ template-parts/content/content-none.php
@@ -14,7 +14,7 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e('Nothing Found', 'twentynineteenchild'); ?></h1>
+		<h1 class="page-title"><?php _e('No he encontrado nada', 'twentynineteenchild'); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
@@ -24,7 +24,7 @@
 			printf(
 				'<p>' . wp_kses(
 					/* translators: 1: link to WP admin new post page. */
-					__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentynineteenchild'),
+					__('¿Preparado para publicar tu primer post? <a href="%1$s">Empieza aquí</a>.', 'twentynineteenchild'),
 					array(
 						'a' => array(
 							'href' => array(),
@@ -37,14 +37,14 @@
 		elseif (is_search()) :
 			?>
 
-		<p><?php _e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentynineteenchild'); ?></p>
+		<p><?php _e('Perdón, no he encontrado nada que coincida con lo que buscas. Puedes intentarlo de nuevo con un término diferente.', 'twentynineteenchild'); ?></p>
 		<?php
 			get_search_form();
 
 		else :
 			?>
 
-		<p><?php _e('It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentynineteenchild'); ?></p>
+		<p><?php _e('Parece que no podemos encontrar lo que estás buscando. Puede que una nueva búsqueda te pueda ayudar. .', 'twentynineteenchild'); ?></p>
 		<?php
 			get_search_form();
 
